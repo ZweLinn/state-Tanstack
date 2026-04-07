3e26a27cc8a969cb41da549416c8ae91727be938
zwelinn <zewlinnmg@gmail.com>
2026-04-07 22:30:24 +0700

    Mutation Example

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import "./App.css";
+import MutationExample from "./components/MutationExample";
 import QueryExample from "./components/QueryExample";
 
 function App() {
   return (
     <main>
       <QueryExample />
+      <br/> 
+      <MutationExample/>
     </main>
   );
 }
--- src/components/MutationExample.tsx
+++ src/components/MutationExample.tsx
@@ -1,7 +1,58 @@
+import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
+
+const createPost = async (post: { title: string; body: string , userId : number }) => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(post),
+  });
+  return res.json();
+}
 export default function MutationExample() {
-    return (
-        <div>
-            This is MutationExample
-        </div>
-    )
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
+  const {mutate , data , isPending , isError , error} = useMutation({mutationFn : createPost})
+  return (
+    <div >
+      <div>This is MutationExample</div>
+
+      <input
+        title="post title"
+        placeholder="post title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+
+      <textarea
+        placeholder="post body"
+        value={body}
+        onChange={(e) => setBody(e.target.value)}
+      />
+      
+      <button onClick={() => mutate({title , body , userId : 1})}>
+        Submit
+      </button>
+
+      {
+        isPending && <p>Creating post...</p>
+      }
+      {
+        isError && <p>Error creating post: {(error as Error).message}</p>
+      }
+      {
+        data && (
+          <div>
+            <h3>Post Created:</h3>
+            <p>ID: {data.id}</p>
+            <p>Title: {data.title}</p>
+            <p>Body: {data.body}</p>
+          </div>
+        )
+      }
+    </div>
+  );
 }
